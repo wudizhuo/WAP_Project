2a032055b294492d6d7db47c9116747920ef5a7a
wudizhuo <sunzhuo1228@126.com>
2017-05-16 16:06:33 -0500

    code login

---
+++ src/com/wap/Login.java
@@ -0,0 +1,65 @@
+package com.wap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Login extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        try {
+            String name = request.getParameter("username");
+            String password = request.getParameter("password");
+            if (validateUser(name, password)) {
+                out.println("<h1>Welcome </h1>" + name);
+            } else {
+                out.println("Login Failure");
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    private boolean validateUser(String name, String passKey) {
+        return true;
+    }
+
+//    protected boolean validateUser(String userName, String passKey) {
+//        connect userAuthenticationConnection = new connect();
+//        ResultSet rs = null;
+//        boolean test = userAuthenticationConnection.doConnection();
+//        if (test == true) {
+//            Connection con = userAuthenticationConnection.getConnect();
+//            PreparedStatement stmt = null;
+//            String sql = "select password from users where user_name = ?";
+//            try {
+//
+//                stmt = con.prepareStatement(sql);
+//                stmt.setString(1, userName);
+//                rs = stmt.executeQuery();
+//                if (!rs.first()) {
+//                    return false;
+//                }
+//            } catch (SQLException e1) {
+//                e1.printStackTrace();
+//            }
+//            String passWord = null;
+//            try {
+//
+//                passWord = rs.getString("password");
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//            if (passWord.equals(passKey)) {
+//                userAuthenticationConnection.closeConnection();
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+}
--- web/login.jsp
+++ web/login.jsp
@@ -6,7 +6,7 @@
 </head>
 <body>
 <h1>Login Form</h1>
-<form action="/login" method="post">
+<form action="login" method="post">
     Username: <input type="text" name="username"/>
     <br>
     Password: <input type="password" name="password"/>
