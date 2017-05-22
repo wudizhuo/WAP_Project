7cad44f4166f2ad7f100e5e31ffe2750f61434f2
wudizhuo <sunzhuo1228@126.com>
2017-05-22 09:26:42 -0500

    refactor code

--- src/com/wap/login/Login.java
+++ src/com/wap/login/Login.java
@@ -8,30 +8,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Login extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        try {
-            String name = request.getParameter("username");
-            String password = request.getParameter("password");
-            User user = new UserRepository((Datastore) this.getServletContext()
-                    .getAttribute("DATA_STORE")).get(name, password);
-            HttpSession session = request.getSession();
-            session.setAttribute("User", user);
-
-            if (user != null) {
-                response.sendRedirect("DashBoard.jsp");
-            } else {
-                out.println("Login Failure");
-            }
-        } finally {
-            out.close();
+        String name = request.getParameter("username");
+        String password = request.getParameter("password");
+        User user = new UserRepository((Datastore) this.getServletContext()
+                .getAttribute("DATA_STORE")).get(name, password);
+        if (user != null) {
+            request.getSession().setAttribute("User", user);
+            response.sendRedirect("DashBoard.jsp");
+        } else {
+            out.println("Login Failure");
         }
     }
 
