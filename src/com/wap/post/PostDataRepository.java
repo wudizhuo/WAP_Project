8b2b6f4119411135f6e34599bf89afc78918fbc3
洋洋李 <cs.yangyangli@gmail.com>
2017-05-21 20:02:06 -0500

    fix bug

--- src/com/wap/post/PostDataRepository.java
+++ src/com/wap/post/PostDataRepository.java
@@ -1,6 +1,8 @@
 package com.wap.post;
 
 import com.wap.common.BaseRepository;
+import com.wap.user.User;
+import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.query.FindOptions;
 import org.mongodb.morphia.query.Query;
@@ -15,8 +17,12 @@ public class PostDataRepository extends BaseRepository {
         super(datastore);
     }
 
-    public List<PostData> getPost() {
+    public List<PostData> getPostData() {
         return datastore.createQuery(PostData.class).order("-_id").asList(new FindOptions().limit(25));
+    }
+
+    public PostData getPost(String Id) {
+        return datastore.get(PostData.class, new ObjectId(Id));
     }
 
     public void save(PostData post) {
